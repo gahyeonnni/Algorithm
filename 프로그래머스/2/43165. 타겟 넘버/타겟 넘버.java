e31d7348b4c2862c421603c284155846508f4827
김가현 <165350514+gahyeonnni@users.noreply.github.com>
2025-01-14 20:31:39 +0900

    [level 2] Title: 타겟 넘버, Time: 283.59 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,29 +1,30 @@
+import java.io.*;
 import java.util.*;
 
 class Solution {
     private Queue<Integer> queue = new LinkedList<>();
     private int answer = 0;
-
-    public void BFS(int[] numbers, int target) {
+    
+    public void BFS(int [] numbers, int n){
         queue.add(0);
-        for (int i = 0; i < numbers.length; i++) {
+        
+        for (int i = 0; i < numbers.length; i++){
             int size = queue.size();
             for (int j = 0; j < size; j++) {
-                int sum = queue.poll(); 
-                queue.add(sum + numbers[i]); 
-                queue.add(sum - numbers[i]); 
-            }
+                int a = queue.poll();
+                queue.add(a + numbers[i]);
+                queue.add(a - numbers[i]);
+            } 
         }
-
-        while (!queue.isEmpty()) {
-            if (queue.poll() == target) {
-                answer++;
-            }
+        
+        while (!queue.isEmpty()){
+         if (queue.poll() == n)
+             answer++;
         }
-    }
-
+      }     
+    
     public int solution(int[] numbers, int target) {
         BFS(numbers, target);
-        return answer; 
+        return answer;
     }
 }
