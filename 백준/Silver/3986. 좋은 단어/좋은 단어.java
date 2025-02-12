2e5e0982dd65352844459657d9097fe754c9c148
김가현 <165350514+gahyeonnni@users.noreply.github.com>
2025-02-12 13:47:38 +0900

    [Silver IV] Title: 좋은 단어, Time: 288 ms, Memory: 22000 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int count = 0;
+        for (int i = 0; i < a; i++){
+            String s = br.readLine();
+            Stack<Character> stack = new Stack<>();
+            for (int j = 0; j < s.length(); j++){
+                if (!stack.isEmpty() && stack.peek() == s.charAt(j))
+                    stack.pop();
+                else
+                    stack.push(s.charAt(j));
+            }
+            if (stack.size() == 0)
+                count++;
+        }
+        System.out.println(count);
+    }
+}
