f3ae1a7b5d1c42357ef1b085715a396127e60769
김가현 <165350514+gahyeonnni@users.noreply.github.com>
2025-02-13 15:03:33 +0900

    [level 2] Title: 프로세스, Time: 0.92 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,36 +1,25 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public int solution(int[] pri, int location) {
-        Queue<int[]> queue = new LinkedList<>();
-        
-        for (int i = 0; i < pri.length; i++) {
-            queue.offer(new int[]{pri[i], i});
-        }
-
         int answer = 0;
-
+        Queue <int []> queue = new LinkedList<>();
+        for (int i = 0; i < pri.length; i++){
+            queue.add(new int [] {pri[i], i});
+        }
         Arrays.sort(pri);
         int max = pri.length - 1;
-
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            
-            if (current[0] == pri[max]) {
-                answer++; 
-                max--; 
-                
-                if (current[1] == location) {
-                    return answer;
-                }
-            } 
-            
-            else {
-                queue.offer(current);
+        while (!queue.isEmpty()){
+            int [] current = queue.poll();
+            if (current[0] == pri[max]){
+                answer++;
+                max--;
+                if (current[1] == location)
+                    break;
             }
+            else
+                queue.add(current);
         }
-
         return answer;
     }
 }
