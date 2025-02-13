47dcf7aa70771435b6b49703d53acaf9831bd4c2
김가현 <165350514+gahyeonnni@users.noreply.github.com>
2025-02-13 11:23:22 +0900

    [level 2] Title: 전화번호 목록, Time: 352.38 ms, Memory: 98.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,14 +1,16 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
+        boolean answer = true;
         Arrays.sort(phone_book);
-        for (int i = 0; i < phone_book.length - 1; i++){
+        
+        for (int i = 0; i < phone_book.length - 1; i++) {
             if (phone_book[i + 1].startsWith(phone_book[i])) {
-                return false; 
+                return false;
             }
         }
-        return true;
+        
+        return answer;
     }
 }
