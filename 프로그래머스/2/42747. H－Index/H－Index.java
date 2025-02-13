72ad299c052bf14af24e7769f186fc395f621cf1
김가현 <165350514+gahyeonnni@users.noreply.github.com>
2025-02-13 10:13:21 +0900

    [level 2] Title: H-Index, Time: 27.45 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,18 +1,18 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    public int solution(int[] cit) {
+    public int solution(int[] citations) {
         int answer = 0;
-        Arrays.sort(cit);
-        for (int i = 0; i < cit[cit.length - 1]; i++){
-            int num = 0;
-            for (int j = 0; j < cit.length; j++){
-                if (cit[j] >= i)
-                    num++;
+        Arrays.sort(citations);
+        for (int i = 0; i < citations[citations.length - 1]; i++){
+            int count = 0;
+            for (int j : citations){
+                if (j >= i)
+                    count++;
+                if (count >= i)
+                    break;
             }
-            if (num >= i)
-                answer = Math.max(answer, i);
+            answer = Math.max(count, answer);
         }
         return answer;
     }
