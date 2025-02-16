49f563005f2220c1101ba9ebcbd8acc144d9db43
김가현 <165350514+gahyeonnni@users.noreply.github.com>
2025-02-16 19:58:03 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 61.27 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,31 +1,33 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int bri, int weight, int[] truck) {
+    public int solution(int bri, int wei, int[] truck) {
         int answer = 0;
-        Queue<Integer> queue = new LinkedList<>();  
-        Queue<int[]> queue2 = new LinkedList<>();   
-        int total = 0;  
-
-        for (int t : truck){
-            queue.add(t);
+        int total = 0;
+        Queue <Integer> queue1 = new LinkedList<>(); //트럭 리스트
+        Queue <int []> queue2 = new LinkedList<>(); //다리에 있는 트럭 
+        
+        for (int x : truck){
+            queue1.add(x);
         }
         
-        while (!queue.isEmpty() || !queue2.isEmpty()){
+        while (!queue1.isEmpty() || !queue2.isEmpty()){
+            
             answer++;
             
             if (!queue2.isEmpty() && queue2.peek()[1] == bri){
                 total -= queue2.poll()[0];
             }
-                
-             if (!queue.isEmpty() && total + queue.peek() <= weight) {
-                int x = queue.poll();
-                queue2.add(new int[]{x, 0});  
-                total += x;
+            
+            if (!queue1.isEmpty() && total + queue1.peek() <= wei){
+                int a = queue1.poll();
+                total += a;
+                queue2.add(new int [] {a, 0});
             }
             
-            for (int [] t : queue2)
-                t[1]++;
+            for (int [] x : queue2){
+                x[1]++;
+            }
         }
         
         return answer;
