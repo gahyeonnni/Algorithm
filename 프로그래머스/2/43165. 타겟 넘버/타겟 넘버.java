4f079935b237191c1d304351ea9b02450204305c
김가현 <165350514+gahyeonnni@users.noreply.github.com>
2025-02-19 09:20:46 +0900

    [level 2] Title: 타겟 넘버, Time: 187.32 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,30 +1,36 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    private Queue<Integer> queue = new LinkedList<>();
-    private int answer = 0;
+    private static int answer = 0;
+    private static boolean [] visit;
     
-    public void BFS(int [] numbers, int n){
-        queue.add(0);
+    private static void CHECK(int [] num, boolean [] visit, int a){
+        Queue <Integer> queue = new LinkedList<>();
         
-        for (int i = 0; i < numbers.length; i++){
-            int size = queue.size();
-            for (int j = 0; j < size; j++) {
-                int a = queue.poll();
-                queue.add(a + numbers[i]);
-                queue.add(a - numbers[i]);
-            } 
+        for (int i = 0; i < num.length; i++){
+            if (queue.isEmpty()){
+                queue.add(num[i]);
+                queue.add(-num[i]);
+            }
+            else {
+                int size = queue.size();
+                for (int j = 0; j < size; j++){
+                    int n = queue.poll();
+                    queue.add(n + num[i]);
+                    queue.add(n - num[i]);
+                }
+            }
         }
         
-        while (!queue.isEmpty()){
-         if (queue.poll() == n)
-             answer++;
+        for (int x : queue){
+            if (x == a)
+                answer++;
         }
-      }     
+    }
     
     public int solution(int[] numbers, int target) {
-        BFS(numbers, target);
+        visit = new boolean [numbers.length];
+        CHECK(numbers, visit, target);
         return answer;
     }
 }
