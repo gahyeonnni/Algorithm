b7ea9f175ff6217b25dfa66ff442e0bee0e97b45
김가현 <165350514+gahyeonnni@users.noreply.github.com>
2025-02-19 09:07:53 +0900

    [level 1] Title: 모의고사, Time: 1.13 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,49 +1,38 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    private int[] index1 = {1, 2, 3, 4, 5};
-    private int[] index2 = {2, 1, 2, 3, 2, 4, 2, 5};
-    private int[] index3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    private static int [] answer1 = {1, 2, 3, 4, 5}; //5개
+    private static int [] answer2 = {2, 1, 2, 3, 2, 4, 2, 5}; //8개
+    private static int [] answer3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; //10개
     
-    private int answer1 = 0;
-    private int answer2 = 0;
-    private int answer3 = 0;
+    private static int ans1 = 0;
+    private static int ans2 = 0;
+    private static int ans3 = 0;
     
     public int[] solution(int[] answers) {
-        
-        int x = answers.length;
-        
-        for (int i = 0; i < answers.length; i++) {
+        for (int i = 0; i < answers.length; i++){
             int a1 = i % 5;
             int a2 = i % 8;
             int a3 = i % 10;
             
-            if (index1[a1] == answers[i])
-                answer1++;
-            if (index2[a2] == answers[i])
-                answer2++;
-            if (index3[a3] == answers[i])
-                answer3++;
+            if (answer1[a1] == answers[i])
+                ans1++;
+            if (answer2[a2] == answers[i])
+                ans2++;
+            if (answer3[a3] == answers[i])
+                ans3++;
         }
-        int [] ans = new int [3];
-        ans[0] = answer1;
-        ans[1] = answer2;
-        ans[2] = answer3;
-        
-        int maxnum = 0;
-        maxnum = Math.max(answer1, Math.max(answer2, answer3));
-        
+        int a = Math.max(ans1, Math.max(ans2, ans3));
         List <Integer> list = new LinkedList<>();
-        for (int i = 0; i < ans.length; i++){
-            if (ans[i] == maxnum)
-                list.add(i+1);
-        }
-        
-        int [] answer = new int [list.size()];
-        for (int i = 0; i < list.size(); i++){
+        if (a == ans1)
+            list.add(1);
+        if (a == ans2)
+            list.add(2);
+        if (a == ans3)
+            list.add(3);
+        int[] answer = new int [list.size()];
+        for (int i = 0; i < list.size(); i++)
             answer[i] = list.get(i);
-        }
         
         return answer;
     }
