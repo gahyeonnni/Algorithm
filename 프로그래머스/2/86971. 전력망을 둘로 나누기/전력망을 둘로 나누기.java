a94a7379ca4a4f453f6a7d12dd65a037b6fce153
김가현 <165350514+gahyeonnni@users.noreply.github.com>
2025-02-20 14:37:28 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 18.02 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,52 +1,51 @@
 import java.util.*;
 
 class Solution {
-    private static ArrayList<Integer>[] graph;
+    private static ArrayList<Integer>[] list;
+    private static boolean [] visit;
+    
+    public static int CHECK(int a, boolean [] visited){
+        visited[a] = true;
+        int count = 1;
+        
+        for (int i : list[a]){
+            if (!visited[i]){
+                count += CHECK(i, visited);
+            }
+        }
+        
+        return count;
+    }
     
     public int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
-
-        graph = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
+        list = new ArrayList [n + 1];
+        for (int i = 1; i <= n; i++){
+            list[i] = new ArrayList<>();
         }
-
-        for (int i = 0; i < wires.length; i++) {
-            int x = wires[i][0];
-            int y = wires[i][1];
-            graph[x].add(y);
-            graph[y].add(x);
+        for (int i = 0; i < wires.length; i++){
+            int [] x = wires[i];
+            int a = x[0];
+            int b = x[1];
+            list[a].add(b);
+            list[b].add(a);
         }
-
-        for (int i = 0; i < wires.length; i++) {
-            int x = wires[i][0];
-            int y = wires[i][1];
-
-            graph[x].remove(Integer.valueOf(y));
-            graph[y].remove(Integer.valueOf(x));
-
-            boolean[] visited = new boolean[n + 1];
-            int networkSize = dfs(1, visited); 
+        for (int i = 0; i < wires.length; i++){
+            int [] x = wires[i];
+            int a = x[0];
+            int b = x[1];
+            list[a].remove(Integer.valueOf(b));
+            list[b].remove(Integer.valueOf(a));
             
-            int diff = Math.abs(networkSize - (n - networkSize));
+            boolean [] visit = new boolean [n + 1];
+            int count = CHECK(1, visit);
+            
+            int diff = Math.abs(count - (n - count));
             answer = Math.min(answer, diff);
-
-            graph[x].add(y);
-            graph[y].add(x);
+            
+            list[a].add(b);
+            list[b].add(a);
         }
-
         return answer;
-    }
-
-    private int dfs(int node, boolean[] visited) {
-        visited[node] = true;
-        int count = 1;
-
-        for (int next : graph[node]) {
-            if (!visited[next]) {
-                count += dfs(next, visited);
-            }
-        }
-        return count;
     }
 }
