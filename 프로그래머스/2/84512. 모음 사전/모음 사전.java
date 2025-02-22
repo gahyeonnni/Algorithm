d9a834de550a2502643730f8b22c6ebdbdbf3751
김가현 <165350514+gahyeonnni@users.noreply.github.com>
2025-02-22 11:42:38 +0900

    [level 2] Title: 모음 사전, Time: 9.35 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -2,19 +2,22 @@ import java.util.*;
 
 class Solution {
     private static String [] alpha = {"A", "E", "I", "O", "U"};
-    public static void make(int a, List <String> list, String x){
+    private static List <String> list;
+    
+    private static void make (int a, String x){
         if (a >= 5)
             return;
         for (int i = 0; i < alpha.length; i++){
-            list.add( x + alpha[i]);
-            make(a + 1, list, x + alpha[i]);
+            list.add(x + alpha[i]);
+            make(a + 1, x + alpha[i]);
         }
     }
+    
     public int solution(String word) {
         int answer = 0;
-        List <String> list = new LinkedList<>();
+        list = new LinkedList<>();
         String x = "";
-        make(0, list, x);
+        make(0, x);
         answer = list.indexOf(word) + 1;
         return answer;
     }
