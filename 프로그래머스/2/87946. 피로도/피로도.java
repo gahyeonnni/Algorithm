37991e74e17b8dda39856334a1e48e1ddd532831
김가현 <165350514+gahyeonnni@users.noreply.github.com>
2025-02-24 10:22:40 +0900

    [level 2] Title: 피로도, Time: 9.41 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,26 +2,23 @@ import java.util.*;
 
 class Solution {
     private static int answer = 0;
-    private static boolean [] visit;
     
-    private static void check(int k, int [][] dungeons, boolean [] visit, int people){
-        answer = Math.max(answer, people);
+    private static void CHECK(int k, boolean [] visit, int [][] dungeons, int a){
+        
+        answer = Math.max(a, answer);
         
         for (int i = 0; i < dungeons.length; i++){
-            if (!visit[i]){
-                int [] x = dungeons[i];
-                if (k >= x[0]){
-                    visit[i] = true;
-                    check(k - x[1], dungeons, visit, people + 1);
-                    visit[i] = false;
-                }
+            if (!visit[i] && k >= dungeons[i][0]) {
+                visit[i] = true;
+                CHECK(k - dungeons[i][1], visit, dungeons, a + 1);
+                visit[i] = false;
             }
         }
     }
+    
     public int solution(int k, int[][] dungeons) {
-        visit = new boolean [dungeons.length];
-        int people = 0;
-        check(k, dungeons, visit, people);
+        boolean [] visit = new boolean [dungeons.length];
+        CHECK(k, visit, dungeons, 0);
         return answer;
     }
 }
