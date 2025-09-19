230e8a62ba94e7b31f5485caaa0a2f7250308e99
김가현 <165350514+gahyeonnni@users.noreply.github.com>
2025-09-19 15:05:41 +0900

    [level 2] Title: 땅따먹기, Time: 72.85 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,25 +1,29 @@
-import java.util.*;
+import java.io.*; 
+import java.util.*; 
 
 class Solution {
     int solution(int[][] land) {
-        
-        int n = land.length;
-        for (int i = 1; i < n; i++) {
+        int answer = 0;
+        int dir; 
+        for (int i = 1; i < land.length; i++) {
             for (int j = 0; j < 4; j++) {
-                int max = 0;
-                for (int k = 0; k < 4; k++) {
-                    if (j != k) {
-                        max = Math.max(max, land[i-1][k]);
+                dir = 0;
+                List <Integer> list = new LinkedList<>(); 
+                while (dir < 4) {
+                    if (dir != j) {
+                        list.add(land[i-1][dir]);   
                     }
+                    dir++; 
                 }
-                land[i][j] += max;
+                int max = 0;
+                for (int val : list) {
+                    max = Math.max(max, val);
+                }
+                land[i][j] += max; 
             }
         }
-
-        int answer = 0;
-        
-        for (int j = 0; j < 4; j++) {
-            answer = Math.max(answer, land[n-1][j]);
+        for (int i = 0; i < 4; i++) {
+            answer = Math.max(answer, land[land.length-1][i]);
         }
         return answer;
     }
