206ca3eca5ab078b03dc63bc53eb830e2380457a
김가현 <165350514+gahyeonnni@users.noreply.github.com>
2025-10-07 15:21:57 +0900

    [Gold III] Title: 게임 개발, Time: 268 ms, Memory: 23128 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -2,58 +2,71 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n, m;
+    static int n;
+    static StringTokenizer st;
+    static List <List <Integer>> list;
+    static boolean check(boolean [] visited) {
+        for (boolean x : visited) {
+            if (!x)
+                return false;
+        }
+        return true;
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        List<Map.Entry<Integer, List<Integer>>> list = new ArrayList<>();
+        n = Integer.parseInt(br.readLine());
+        list = new ArrayList<>();
+        int [] array = new int [n];
+        for (int i = 0; i < n; i++)
+            list.add(new ArrayList<>());
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            List<Integer> temp = new ArrayList<>();
-            while (true) {
-                int val = Integer.parseInt(st.nextToken());
-                if (val == -1)
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            array[i] = a;
+            while (st.hasMoreTokens()) {
+                int x = Integer.parseInt(st.nextToken());
+                if (x == -1)
                     break;
-                temp.add(val);
-            }
-            if (!temp.isEmpty()) {
-                int key = temp.get(0);
-                List<Integer> value = new ArrayList<>(temp.subList(1, temp.size()));
-                list.add(new AbstractMap.SimpleEntry<>(key, value));
+                list.get(i).add(x - 1);
             }
         }
-        int [] hour = new int [n];
-        boolean [] check = new boolean [n];
+
+        boolean [] visited = new boolean [n];
+        for (int i = 0; i < n; i++)  {
+            if (list.get(i).size() == 0)
+                visited[i] = true;
+        }
 
         while (true) {
-            boolean c = false;
+            if (check(visited))
+                break;
 
             for (int i = 0; i < n; i++) {
-                if (check[i])
+                if (visited[i])
                     continue;
-                Map.Entry<Integer, List<Integer>> map = list.get(i);
-                List<Integer> val = map.getValue();
 
-                int max = 0;
-                boolean ready = true;
-                for (int dep : val) {
-                    if (!check[dep - 1]) {
-                        ready = false;
+                List <Integer> x = list.get(i);
+                boolean check = true;
+
+                for (int j : x) {
+                    if (!visited[j]) {
+                        check = false;
                         break;
                     }
-                    max = Math.max(max, hour[dep - 1]);
                 }
-                if (ready) {
-                    hour[i] = max + map.getKey();
-                    check[i] = true;
-                    c = true;
+
+                if (check) {
+                    int max = 0;
+                    for (int j : x) {
+                        max = Math.max(max, array[j]);
+                    }
+                    array[i] += max;
+                    visited[i] = true;
                 }
             }
-                    if (!c)
-                        break;
-            }
-            for (int a : hour) {
-                System.out.println(a);
-            }
+        }
+        for (int a : array)
+            System.out.println(a);
     }
 }
+
