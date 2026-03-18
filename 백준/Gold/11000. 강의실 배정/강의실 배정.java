fd97ba62a77fa117abe5ede601dd7e55f32a25da
김가현 <hyeonnni@naver.com>
2026-03-18 23:32:36 +0900

    [Gold IV] Title: 강의실 배정, Time: 584 ms, Memory: 67952 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static StringTokenizer st;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            arr[i][0] = b;
+            arr[i][1] = c;
+        }
+
+        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            if (!pq.isEmpty() && pq.peek() <= arr[i][0]) {
+                pq.poll();
+            }
+            pq.offer(arr[i][1]);
+        }
+        System.out.println(pq.size());
+    }
+}
