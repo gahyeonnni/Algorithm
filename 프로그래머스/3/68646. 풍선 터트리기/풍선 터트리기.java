5d548dfddbe88bb7d08adb865a9e8d082249998e
김가현 <hyeonnni@naver.com>
2026-06-10 14:53:44 +0900

    [level 3] Title: 풍선 터트리기, Time: 22.37 ms, Memory: 155 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -3,35 +3,24 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] a) {
+        int answer = 0; 
         int n = a.length;
         
-        if (n <= 2) 
-            return n;
-
-        int[] left = new int[n];  
-        int[] right = new int[n]; 
+        int [] left = new int [n]; 
+        int [] right = new int [n]; 
         
         left[0] = a[0]; 
         for (int i = 1; i < n; i++)
-            left[i] = Math.min(left[i - 1], a[i]); 
+            left[i] = Math.min(a[i], left[i - 1]); 
         
         right[n - 1] = a[n - 1]; 
-        for (int i = n - 2; i >= 0; i--) {
-            right [i] = Math.min(right[i + 1], a[i]);
-        }
+        for (int j = n - 2; j >= 0; j--)
+            right[j] = Math.min(a[j],  right[j + 1]); 
         
-        int answer = 0;
+        for (int i = 0; i < n; i++)
+            if (a[i] <= left[i] || a[i] <= right[i])
+                answer++; 
         
-        for (int i = 0; i < n; i++) {
-            if (a[i] <= left[i] || a[i] <= right[i]) {
-                answer++;
-            }
-        }
-
         return answer;
     }
 }
-
-
-
-
