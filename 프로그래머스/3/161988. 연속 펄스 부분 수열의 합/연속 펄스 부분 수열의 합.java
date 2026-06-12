6b2a1b620841015d3f4e3c88656906b8da4ce0e8
김가현 <hyeonnni@naver.com>
2026-06-12 16:40:28 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 11.04 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -2,35 +2,20 @@ import java.io.*;
 import java.util.*; 
 
 class Solution {
-    static long maxsum(int [] array) {
-        long cur = array[0];
-        long max = array[0]; 
-        
-        for (int i = 1; i < array.length; i++) {
-            cur = Math.max(array[i], cur + array[i]); 
-            max = Math.max(max, cur);
-        }
-        
-        return max;
-    }
-    public long solution(int[] sequence) {
+    public long solution(int[] seq) {
         long answer = 0;
-        int [] copy1 = Arrays.copyOf(sequence, sequence.length); 
-        int cur = 1;
-        for (int i = 0; i < copy1.length; i++) {
-            copy1[i] = copy1[i] * cur; 
-            cur *= -1;
+        
+        long sum1 = 0; 
+        long sum2 = 0; 
+        
+        for (int i = 0; i < seq.length; i++) {
+            long pulse1 = (long) seq[i] * (i % 2 == 0 ? 1 : -1);
+            long pulse2 = (long) seq[i] * (i % 2 == 0 ? -1 : 1);
+            
+            sum1 = Math.max(pulse1, sum1 + pulse1); 
+            sum2 = Math.max(pulse2, sum2 + pulse2);
+            answer = Math.max(answer, Math.max(sum1, sum2));
         }
-        int [] copy2 = Arrays.copyOf(sequence, sequence.length); 
-        cur = -1;
-        for (int i = 0; i < copy1.length; i++) {
-            copy2[i] = copy2[i] * cur; 
-            cur *= -1;
-        } 
-        
-        long max1 = maxsum(copy1); 
-        long max2 = maxsum(copy2); 
-        
-        return Math.max(max1, max2);
+        return answer;
     }
 }
