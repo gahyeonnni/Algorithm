e9d2b010bfe3c5f147fa140948bbba0428451537
김가현 <hyeonnni@naver.com>
2026-06-22 17:33:38 +0900

    [level 3] Title: 단어 변환, Time: 0.41 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,42 +1,56 @@
-import java.util.*;
+import java.io.*;
+import java.util.*; 
 
-class Solution {
-    public int solution(String begin, String target, String[] words) {
-        boolean[] visited = new boolean[words.length];
-        return BFS(begin, target, words, visited);
+class Solution { 
+    
+    static boolean oneWordChange(String begin, String target) {
+        int count = 0;
+        for (int i = 0; i < begin.length(); i++) {
+            if (begin.charAt(i) != target.charAt(i))
+                count++;
+        }
+        if (count == 1)
+            return true; 
+        else 
+            return false;
     }
-
-    private static int BFS(String a, String b, String[] words, boolean[] visited) {
-        Queue<String> queue = new LinkedList<>();
-        Queue<Integer> depthQueue = new LinkedList<>(); 
-        
-        queue.add(a);
-        depthQueue.add(0);
+    
+    static int bfs(String begin, String target, String[] words, boolean[] check) {
+        Queue<String> queue = new LinkedList<>(); 
+        queue.add(begin); 
+        int count = 0;
         
         while (!queue.isEmpty()) {
-            String x = queue.poll();
-            int depth = depthQueue.poll(); 
+            int size = queue.size(); 
             
-            if (x.equals(b)) 
-                return depth; 
-            
-            for (int i = 0; i < words.length; i++) {
-                if (!visited[i]) {
-                    int diff = 0;
-                    for (int j = 0; j < x.length(); j++) {
-                        if (x.charAt(j) != words[i].charAt(j))
-                            diff++;
-                        if (diff > 1) break;
-                    }
-                    
-                    if (diff == 1) { 
-                        visited[i] = true;
-                        queue.add(words[i]);
-                        depthQueue.add(depth + 1); 
+            for (int s = 0; s < size; s++) {
+                String x = queue.poll();
+                
+                if (x.equals(target))
+                    return count; 
+                
+                for (int i = 0; i < words.length; i++) {
+                    if (!check[i]) {
+                        String word = words[i]; 
+                        if (oneWordChange(x, word)) {
+                            queue.add(word); 
+                            check[i] = true; 
+                        }
                     }
                 }
             }
+            count++; 
         }
-        return 0; 
+        
+        return 0;
+    }
+    
+    public int solution(String begin, String target, String[] words) {
+        if (!java.util.Arrays.asList(words).contains(target)) 
+            return 0; 
+        
+        boolean[] check = new boolean[words.length];
+        
+        return bfs(begin, target, words, check);
     }
 }
