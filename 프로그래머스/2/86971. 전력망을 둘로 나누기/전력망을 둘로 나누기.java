2461b15d16a33149f2a41db55106b4c0bfaa18bc
김가현 <hyeonnni@naver.com>
2026-06-30 11:30:40 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.03 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,55 +1,64 @@
-import java.util.*;
+import java.io.*; 
+import java.util.*; 
 
 class Solution {
-    private static ArrayList <Integer> [] list;
-    private static int answer = Integer.MAX_VALUE;
-    
-    private static int BFS(int a, boolean [] visited){
+    static int count(int start, List<List<Integer>> list, boolean [] check) {
         Queue <Integer> queue = new LinkedList<>();
-        visited[a] = true;
-        queue.add(a);
+        queue.add(start);
+        check[start] = true; 
+        int total = 0;
         
-        int count = 0;
-        
-        while (!queue.isEmpty()){
-            int x = queue.poll();
-            count++;
-            
-            for (int next : list[x]){
-                if (!visited[next]){
-                    visited[next] = true;
-                    queue.add(next);
+        while (!queue.isEmpty()) {
+            int q = queue.poll(); 
+            total++;
+            for (int a : list.get(q)) {
+                if (!check[a]) {
+                    queue.add(a); 
+                    check[a] = true;
                 }
             }
         }
-        return count;
+        
+        return total;
+    }
+    
+    static int diff(List<List<Integer>> list, int n, int start, int finish) {
+        boolean [] check = new boolean [n + 1]; 
+        check[finish] = true;
+        int half1 = count(start, list, check);
+        
+        boolean [] check2 = new boolean [n + 1]; 
+        check2[start] = true;
+        int half2 = count(finish, list, check2);
+        
+        if (half1 + half2 != n)
+            return Integer.MAX_VALUE;
+        
+        return Math.abs(half1 - half2);
+        
     }
     
     public int solution(int n, int[][] wires) {
-        list = new ArrayList[n+1];
-        for (int i = 1; i <= n; i++)
-            list[i] = new ArrayList<>();
-        
-        for (int i = 0; i < wires.length; i++){
-            int [] x = wires[i];
-            list[x[0]].add(x[1]);
-            list[x[1]].add(x[0]);
+        int answer = Integer.MAX_VALUE;
+        List<List<Integer>> list = new ArrayList<>(); 
+        for (int i = 0; i < n + 1; i++){
+            list.add(new ArrayList<>()); 
         }
         
-        for (int i = 0; i < wires.length; i++) {
-            int [] x = wires[i];
+        for (int [] wire : wires)  {
+            int start = wire[0]; 
+            int finish = wire[1]; 
+            list.get(start).add(finish);
+            list.get(finish).add(start);
+        }
+
+        for (int i = 0; i < n + 1; i++) {
+            List <Integer> lis = list.get(i); 
+            if (lis.size() == 0)
+                continue; 
             
-            list[x[0]].remove(Integer.valueOf(x[1]));
-            list[x[1]].remove(Integer.valueOf(x[0]));
-            
-            boolean [] visited = new boolean [n+1];
-            int count = BFS(1, visited);
-            
-            int diff = Math.abs(count - (n - count));
-            answer = Math.min(diff, answer);
-            
-            list[x[0]].add(x[1]);
-            list[x[1]].add(x[0]);
+            for (int a : lis)
+                answer = Math.min(answer, diff(list, n, i, a));
         }
         
         return answer;
