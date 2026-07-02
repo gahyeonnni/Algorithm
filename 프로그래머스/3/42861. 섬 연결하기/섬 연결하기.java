f5c3ee6b5b8052cc54370df98adeb51aad0ee113
김가현 <hyeonnni@naver.com>
2026-07-02 11:08:21 +0900

    [level 3] Title: 섬 연결하기, Time: 6.01 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -2,52 +2,71 @@ import java.io.*;
 import java.util.*; 
 
 class Solution {
-    static int bfs(Map <Integer, List<Integer []>> map, int start, int n) {
-        boolean [] visited = new boolean [n]; 
-        PriorityQueue <int []> pq = new PriorityQueue<>(
-            (a,b) -> a[1] - b[1]
-        );
-        pq.add(new int[]{start, 0});
-        int totalCost = 0;
-        int count = 0;
-        
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int node = cur[0];
-            int cost = cur[1];
-            
-            if (visited[node])
-                continue; 
-            
-            visited[node] = true; 
-            totalCost += cost; 
-            count++; 
-            
-            if (count == n) 
-                break; 
-            
-            for (Integer[] next : map.get(node)) {
-                int nextNode = next[0];
-                int nextCost = next[1];
-                if (!visited[nextNode]) {
-                    pq.add(new int[]{nextNode, nextCost});
-                }
+    static List <List <int []>> list;
+    
+    static boolean allArrive(boolean[] check) {
+        for (boolean k : check) {
+            if (!k) {
+                return false; 
             }
         }
-        return totalCost; 
+        return true; 
     }
-    public int solution(int n, int[][] costs) {
-        Map <Integer, List<Integer []>> map = new HashMap<>();
-        for (int i = 0; i <= n; i++)
-            map.put(i, new ArrayList<>());
-        for (int[] a : costs) {
-            int x = a[0];
-            int y = a[1];
-            int z = a[2];
-
-            map.get(x).add(new Integer[]{y, z});
-            map.get(y).add(new Integer[]{x, z});
+    
+    static int totalCount(int start, boolean [] check, int sum) {
+        PriorityQueue<int[]> queue = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
+        queue.add(new int [] {start, 0});
+        
+        while (!queue.isEmpty()) {
+            if (allArrive(check))
+                return sum;
+            
+            int[] current = queue.poll();
+            int q = current[0];
+            int cost = current[1];
+            
+            if (check[q]) 
+                continue;
+            
+            check[q] = true;
+            sum += cost;
+            
+            for (int [] lis : list.get(q)) {
+                int a = lis[0]; 
+                int b = lis[1]; 
+                
+                if (!check[a]) {
+                    queue.add(new int [] {a,b});
+                }
+            }
+            
         }
-        return bfs(map, 0, n);
+        
+        if (!allArrive(check))
+            return Integer.MAX_VALUE; 
+        
+        return sum;
+    }
+    
+    public int solution(int n, int[][] costs) {
+        int answer = Integer.MAX_VALUE; 
+        list = new ArrayList<>(); 
+        for (int i = 0; i < n; i++)
+            list.add(new ArrayList<>());
+        
+        for (int [] cost : costs) {
+            int start = cost[0]; 
+            int dest = cost[1];
+            int count = cost[2]; 
+            list.get(start).add(new int [] {dest, count});
+            list.get(dest).add(new int [] {start, count});
+        }
+        
+        for (int i = 0; i < n; i++) {
+            boolean [] check = new boolean [n];
+            answer = Math.min(answer, totalCount(i, check, 0));
+        }
+            
+        return answer;
     }
 }
