452a707fb4957cdf8a4367af3998f08654b51adf
김가현 <hyeonnni@naver.com>
2026-07-06 11:26:11 +0900

    [level 2] Title: 조이스틱, Time: 0.04 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,28 +1,24 @@
 import java.io.*; 
 import java.util.*; 
 
-
 class Solution {
-    static char a = 'a'; 
-    static char z = 'z';
     public int solution(String name) {
-        int answer = 0; 
+        int answer = 0;
+        int length = name.length(); 
+        int move = length - 1; 
         
-        for (int i = 0; i < name.length(); i++) {
-            char c = name.charAt(i);
-            int diff = Math.min(c - 'A', 'Z' - c + 1);
-            answer += diff;
-        }
-         
-        int move = name.length() - 1; 
-        for (int i = 0; i < name.length(); i++) {
-            int next = i + 1; 
-            while (next < name.length() && name.charAt(next) == 'A')
+        for (int i = 0; i < length; i++) {
+            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
+            
+            int next = i + 1;
+            while (next < length && name.charAt(next) == 'A') {
                 next++;
-            move = Math.min(move, i + name.length() - next + Math.min(i, name.length() - next));
+            }
+            
+            move = Math.min(move, (i * 2) + (length - next));
+            move = Math.min(move, (length - next) * 2 + i);
         }
         
-        answer += move;
-        return answer;
+        return answer + move;
     }
 }
