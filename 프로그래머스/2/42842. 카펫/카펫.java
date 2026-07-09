f0be11f6ede88bac3afe347eb93fe7ffef6a075a
김가현 <hyeonnni@naver.com>
2026-07-09 13:36:01 +0900

    [level 2] Title: 카펫, Time: 3.62 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,14 +1,15 @@
-import java.util.*;
+import java.io.*; 
+import java.util.*; 
 
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int [2];
-        int a = brown + yellow;
-        for (int i = 2; i < (a / 2); i++){
-            if (a % i == 0){ 
-                int b = a / i; //가로 
-                if (i - 2 > 0 && b -2 > 0 && (i -2) * (b - 2) == yellow){
-                    answer[0] = i;
+        int a = brown + yellow; 
+        for (int i = 2; i < (a / 2); i++) {
+            if (a % i == 0) {
+                int b = a / i; 
+                if (i - 2 > 0 && b - 2 > 0 && (i - 2) * (b - 2) == yellow) {
+                    answer[0] = i; 
                     answer[1] = b;
                 }
             }
